e18f13a08c7eb7ed5d9c898802a797f6c46ece1b
veronica17-web <veronicamiryala001@gmail.com>
2023-08-28 16:12:02 +0530

    added register and login api

--- src/controller/enquiryController.js
+++ src/controller/enquiryController.js
@@ -1,38 +1,48 @@
-const enquiryModel = require("../model/enquiryModel")
-const moment = require("moment")
-require("moment-timezone")
-let enquiryForm = async (req,res)=>{
-    try {
-        let data = req.body
-        moment.tz.setDefault("Asia/Kolkata");
+const enquiryModel = require("../model/enquiryModel");
+const moment = require("moment");
+require("moment-timezone");
+let enquiryForm = async (req, res) => {
+  try {
+    let data = req.body;
+    moment.tz.setDefault("Asia/Kolkata");
 
-        // Get the current date and time
-        let date = moment().format("YYYY/MM/DD");
-        let time = moment().format("HH:mm:ss");
-    
-        // Add date and time to the data object
-        data.date = date;
-        data.time = time;
-        let saveData = await enquiryModel.create(data);
+    // Get the current date and time
+    let date = moment().format("YYYY-MM-DD");
+    let time = moment().format("HH:mm:ss");
 
-        // Return the successful response
-        res.status(201).send({ status: true, data: saveData });
+    // Add date and time to the data object
+    data.date = date;
+    data.time = time;
+    let saveData = await enquiryModel.create(data);
 
-    } catch (error) {
-        res.status(500).send({status:false, message: error.message})
+    // Return the successful response
+    res.status(201).send({ status: true, data: saveData });
+  } catch (error) {
+    res.status(500).send({ status: false, message: error.message });
+  }
+};
+
+const getEnquiries = async (req, res) => {
+  try {
+    let filter = req.query;
+    if (Object.keys(filter).length == 0) {
+      let data = await enquiryModel
+        .find({ isDeleted: false })
+        .sort({createdAt : -1});
+      if (!data) {
+        return res.status(404).send({ status: true, message: "no data found" });
+      }
+      return res.status(200).send({ status: true, data: data });
+    } else {
+      let data = await enquiryModel.find( filter );
+      if (!data) {
+        return res.status(404).send({ status: true, message: "no data found" });
+      }
+      return res.status(200).send({ status: true, data: data });
     }
-}
+  } catch (error) {
+    res.status(500).send({ status: false, message: error.message });
+  }
+};
 
-const getEnquiries = async (req,res)=>{
-    try {
-        let data = await enquiryModel.find({isDeleted:false})
-        if(!data){
-            return res.status(404).send({status:true,message:"no data found"})
-        }
-        return res.status(200).send({status:true,data:data})
-    } catch (error) {
-        res.status(500).send({status:false, message: error.message})
-    }
-}
-
-module.exports ={enquiryForm , getEnquiries}
+module.exports = { enquiryForm, getEnquiries };
--- src/router/route.js
+++ src/router/route.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const {enquiryForm , getEnquiries} =require("../controller/enquiryController")
+const {register , login} =require("../controller/userController")
 
 router.get("/test-me", function (req, res) {
     res.send("this is successfully created");
   });
-
+//=============================================================================
   router.post("/enquiryForm",enquiryForm)
   router.get("/getEnquiries",getEnquiries)
+  //===========================================================================
+  router.post("/register",register)
+  router.post("/login",login)
 module.exports = router;
